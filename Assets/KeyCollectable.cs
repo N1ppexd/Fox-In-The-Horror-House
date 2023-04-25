5220667fb25838fde4afa0ba4277fa5785594c0e
N1ppexd <niilohanni@gmail.com>
2023-04-25 11:00:20 +0300

    avain seuraa kettua

--- Assets/KeyCollectable.cs
+++ Assets/KeyCollectable.cs
@@ -10,12 +10,33 @@ namespace HorrorFox
 
         [SerializeField] private ParticleSystem takeKeyParticle;
 
+        [SerializeField] private Transform target; //target on se, jota seurataan, eli pelaaja....
+
+
+
+        private bool hasBeenTaken;
+
+        //millä offsetilla avain seuraa pelaajaa..
+        [SerializeField] private Vector3 keyMovementOffset;
+        [SerializeField] private float movementSpeed, smoothSpeed;//smoothspeed on se, kuinka nopeaa avain seuraa pelaajaa....
+
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.CompareTag("fox"))
             {
+                hasBeenTaken = true;
                 takeKeyParticle.Play();
             }
+        }
+
+
+        private void LateUpdate()
+        {
+            if(!hasBeenTaken)
+                return;
+
+            transform.position = Vector3.Lerp(transform.position, target.position + keyMovementOffset, movementSpeed * Time.deltaTime);
         }
     }
 }
